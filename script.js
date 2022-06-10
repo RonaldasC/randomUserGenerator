13184cd66485972826df51c342b121a2177855dc
RonaldasC <ronaldasr@gmail.com>
2022-06-10 21:26:04 +0300

    pridėtas generavimas pagal įvestą reikšmę

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 'use strict'
 
-const mygtukas = document.getElementById('clickMe').addEventListener('click', generate);
+document.getElementById('clickMe').addEventListener('click', generate);
+document.getElementById('clickYou').addEventListener('click', kiekReiks);
 
 function randomDate(start, end) {
     return new Date(start.getFullYear() + Math.random() * (end.getTime() - start.getTime()));
-}
+};
 function getRandomInt(value) {
   	return Math.floor(Math.random() * (value));
-}
+};
 function generate(){
 	const first_name = ["James", "Robert", "John", "Michael", "David", "William", "Richard", "Joseph", "Thomas", "Charles", "Christopher", "Daniel", "Matthew", "Anthony", "Mark", "Donald", "Steven", "Paul", "Andrew", "Joshua", "Kenneth", "Kevin", "Brian", "George", "Timothy", "Ronald", "Edward", "Jason", "Jeffrey", "Ryan", "Jacob", "Gary", "Nicholas", "Eric", "Jonathan", "Stephen", "Larry", "Justin", "Scott", "Brandon", "Benjamin", "Samuel", "Gregory", "Alexander", "Frank", "Patrick", "Raymond", "Jack", "Dennis", "Jerry", "Tyler", "Aaron", "Jose", "Adam", "Nathan", "Henry", "Douglas", "Zachary", "Peter", "Kyle", "Ethan", "Walter", "Noah", "Jeremy", "Christian", "Keith", "Roger", "Terry", "Gerald", "Harold", "Sean", "Austin", "Carl", "Arthur", "Lawrence", "Dylan", "Jesse", "Jordan", "Bryan", "Billy", "Joe", "Bruce", "Gabriel", "Logan", "Albert", "Willie", "Alan", "Juan", "Wayne", "Elijah", "Randy", "Roy", "Vincent", "Ralph", "Eugene", "Russell", "Bobby", "Mason", "Philip", "Louis","Mary", "Patricia", "Lori", "", "Jennifer", "Linda", "Elizabeth", "Barbara", "Susan", "Jessica", "Sarah", "Karen", "Lisa", "Nancy", "Betty", "Margaret", "Sandra", "Ashley", "Kimberly", "Emily", "Donna", "Michelle", "Carol", "Amanda", "Dorothy", "Melissa", "Deborah", "Stephanie", "Rebecca", "Sharon", "Laura", "Cynthia", "Kathleen", "Amy", "Angela", "Shirley", "Anna", "Brenda", "Pamela", "Emma", "Nicole", "Helen", "Samantha", "Katherine", "Christine", "Debra", "Rachel", "Carolyn", "Janet", "Catherine", "Maria", "Heather", "Diane", "Ruth", "Julie", "Olivia", "Joyce", "Virginia", "Victoria", "Kelly", "Lauren", "Christina", "Joan", "Evelyn", "Judith", "Megan", "Andrea", "Cheryl", "Hannah", "Jacqueline", "Martha", "Gloria", "Teresa", "Ann", "Sara", "Madison", "Frances", "Kathryn", "Janice", "Jean", "Abigail", "Alice", "Julia", "Judy", "Sophia", "Grace", "Denise", "Amber", "Doris", "Marilyn", "Danielle", "Beverly", "Isabella", "Theresa", "Diana", "Natalie", "Brittany", "Charlotte", "Marie", "Kayla", "Alexis", 
     "Lor"];
@@ -44,3 +45,13 @@ function generate(){
     }
     console.log(`${name} ${surname}, ${newDate()}, ${country}`);
 }
+
+// sugeneruot 1000 000 užtruko 105381ms kas yra ~1,7 min
+function kiekReiks() {
+    let a = document.getElementById('kiekis').value;
+    let i = 0;
+        while (i < a) {
+            generate();
+            i++;
+    }  
+};
