e7b04a904a4b769841d1fba406b69c5d537013bc
RonaldasC <ronaldasr@gmail.com>
2022-06-11 22:48:10 +0300

    let changed to const at first_name

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function generate(){
 	const last_name = ["Smith", "Johnson", "Williams", "Brown", "Jones", "Garcia", "Miller", "Davis", "Rodriguez", "Martinez", "Hernandez", "Lopez", "Gonzales", "Wilson", "Anderson", "Thomas", "Taylor", "English", "Moore", "Jackson", "English", "Martin", "Lee", "Perez", "Thompson", "White", "Harris", "Sanchez", "Ramirez", "Lewis", "Robinson", "Walker", "Young", "Allen", "King", "English", "Wright", "Scott", "Torres", "Nguyen", "Hill", "Flores", "Spanish", "Green", "Adams", "Nelson", "Baker", "Hall", "Rivera", "Campbell", "Mitchell", "Carter", "Roberts", "Gomez", "Phillips", "Evans", "Turner", "Diaz", "Parker", "Cruz", "Edwards", "Collins", "Reyes", "Stewart", "Morris", "Morales", "Murphy", "Cook", "Rogers", "Gutierrez", "Ortiz", "Morgan", "Cooper", "Peterson", "Bailey", "Reed", "Kelly", "Howard", "Ramos", "Kim", "Cox", "Ward", "Richardson", "Watson", "Brooks", "Chavez", "Wood", "James", "Bennet", "Gray", "Mendoza", "Ruiz", "Hughes", "Price", "Alvarez", "Castillo", "Sanders", "Patel", "Myers", "Long", "Ross", "Foster", "Jimenez"];
     const countries = ["Poland","Afghanistan","Jamaica","Japan"]
 
-	let name = first_name[getRandomInt(first_name.length)];
+	const name = first_name[getRandomInt(first_name.length)];
 	const surname = last_name[getRandomInt(last_name.length)];
 	const date = randomDate(new Date(2012, 0, 1), new Date());
     const country = countries[getRandomInt(countries.length)];
